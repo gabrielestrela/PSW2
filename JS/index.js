583a907f3f9c7c89321f266a0c902051cffc93d8
gabrielestrela <gabrielestrela92@gmail.com>
2017-06-23 20:48:13 -0300

    Finished Index Page

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    var item = $(".carousel .item");
+    var windowHeight = $(window).height();
+
+    item.eq(0).addClass('active');
+    item.height(windowHeight);
+    item.addClass('full-screen');
+
+    $('.carousel img').each(function () {
+        var $src = $(this).attr('src');
+        var $color = $(this).attr('data-color');
+        $(this).parent().css({
+            'background-image': 'url(' + $src + ')',
+            'background-color': $color
+        });
+        $(this).remove();
+    });
+
+    $(window).on('resize', function () {
+        $wHeight = $(window).height();
+        $item.height($wHeight);
+    });
+
+    $('.carousel').carousel({
+        interval: 6000,
+        pause: "false"
+    });
+});
